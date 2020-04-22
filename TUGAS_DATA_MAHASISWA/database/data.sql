9bff1b39a9281623fcc940c3761ed8f36ec7fb46
Ecinos Hutaa <47842440+ecinoshuta@users.noreply.github.com>
2020-04-22 12:23:04 +0700

    Add files via upload

---
+++ TUGAS_DATA_MAHASISWA/database/data.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Apr 2020 pada 07.13
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data`
+--
+
+CREATE TABLE `data` (
+  `nim` int(9) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data`
+--
+
+INSERT INTO `data` (`nim`, `nama`, `alamat`) VALUES
+(123180001, 'Leni Andriani', 'Riau'),
+(123180002, 'Rusdi Lubis', 'Medan'),
+(123180003, 'rozi', 'bengkulu'),
+(123180004, 'wilian', 'medan'),
+(123180012, 'Faiz Faza Firdaus', 'Jawa Barat');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
